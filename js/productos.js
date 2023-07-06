3a427bbd7288ad2c13b3de891a29c4528be09145
NicolasLaime <nicolaime100@gmail.com>
2023-07-06 18:00:47 -0300

    terminamos3proyectojs

---
+++ js/productos.js
@@ -0,0 +1,37 @@
+const producto = [
+    {
+        id: 1,
+        nombre: "remera vans",
+        precio: 50,
+        Img: "img/remeravans.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Conjunto adidas",
+        precio: 500,
+        Img:"img/Conjuntoadidas.jpg",
+        cantidad: 1,
+
+    },
+
+    {
+        id:3,
+        nombre: "campera tnf",
+        precio: 1000,
+        Img:"img/campera-tnf-invierno.jpg",
+        cantidad: 1,
+
+
+    },
+    {
+        id: 4,
+        nombre: "buzo nike",
+        precio: 400,
+        Img:"img/buzonike.jpg",
+        cantidad: 1,
+
+
+    }
+
+];
